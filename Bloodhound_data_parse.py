a916019060f13ab79e4a9e11a6532ae0bbf7edde
mabangde <linuxseclab@gmail.com>
2023-03-23 23:45:58 +0800

    Update Bloodhound_data_parse.py

--- Bloodhound_data_parse.py
+++ Bloodhound_data_parse.py
@@ -3,24 +3,6 @@ import json
 import re
 import csv
 import datetime
-import sys
-
-def print(*args, **kwargs):
-    built_in_print(*args, **kwargs)
-    output_dir = 'output'
-    # 检查目录是否存在，不存在则创建
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    # 创建output目录
-    output_dir = os.path.join(os.getcwd(), output_dir)
-    file_path = os.path.join(output_dir, 'output.txt')
-    with open(file_path, "a", encoding="utf-8") as f:
-        built_in_print(*args, file=f, **kwargs)
-
-built_in_print = __builtins__.print
-
-# 保存系统自带的print函数
-
 
 ## 首字母大写转换
 def transform_string(s):
@@ -55,6 +37,7 @@ def get_json_data(objlabel):
         'user': '',
         'group': '',
         'ou': '',
+        'domain': ''
     }
     for file in os.listdir(json_file_path):
         if file.endswith('.json'):
@@ -66,6 +49,8 @@ def get_json_data(objlabel):
                 file_mapping['group'] = os.path.join(json_file_path, file)
             elif 'ous' in file:
                 file_mapping['ou'] = os.path.join(json_file_path, file)
+            elif 'domains' in file:
+                file_mapping['domain'] = os.path.join(json_file_path, file)
 
     # 检查请求的对象类型是否在文件映射中
     if objlabel not in file_mapping:
@@ -138,52 +123,61 @@ def write_file(file_name, content):
     # 创建output目录
     output_dir = os.path.join(os.getcwd(), output_dir)
     file_path = os.path.join(output_dir, file_name)
-    print(f"写入文件: {file_path}")
-    print(content)
+    print(f"[+] 写入文件: {file_path}")
+    #print(content)
     with open(file_path, mode='a', encoding='utf-8') as f:
         f.write(content + '\n')
 
 
-def write_csv(file_name, header, data,  encoding= 'utf-8'):
-    """
-    将ACL信息写入CSV文件
-    :param file_name: 文件名，不需要路径
-    :param header: CSV文件头部
-    :param data: 要写入的内容
-    :return: None
-    """
-
+def write_csv(file_name, header, data, encoding='utf-8'):
     output_dir = 'output'
-    # 检查目录是否存在，不存在则创建
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    # 创建output目录
     output_dir = os.path.join(os.getcwd(), output_dir)
 
-
-    #print(f"写入文件: {file_path}")
-    # 生成文件名
     file_path = os.path.join(output_dir, file_name)
-    with open(file_path, mode='a', encoding= encoding, newline='') as file:
-        writer = csv.writer(file)
+    with open(file_path, mode='a', encoding=encoding, newline='') as file:
+        if data and isinstance(data[0], dict):
+            writer = csv.DictWriter(file, fieldnames=header)
+            write_row_func = writer.writerow
+        else:
+            writer = csv.writer(file)
+            write_row_func = writer.writerow
+
         if os.stat(file_path).st_size == 0:
-            writer.writerow(header)
+            if isinstance(writer, csv.DictWriter):
+                writer.writeheader()
+            else:
+                writer.writerow(header)
+
         for row in data:
             try:
-                writer.writerow(row)
+                write_row_func(row)
             except UnicodeEncodeError:
-                filtered_row = []
-                for cell in row:
-                    if isinstance(cell, str):
-                        filtered_row.append(cell.encode('gbk', 'ignore').decode('gbk'))
-                    else:
-                        filtered_row.append(cell)
+                if isinstance(row, dict):
+                    filtered_row = {}
+                    for key, value in row.items():
+                        if isinstance(value, str):
+                            filtered_row[key] = value.encode('gbk', 'ignore').decode('gbk')
+                        else:
+                            filtered_row[key] = value
+                    write_row_func(filtered_row)
+                else:
+                    filtered_row = []
+                    for cell in row:
+                        if isinstance(cell, str):
+                            filtered_row.append(cell.encode('gbk', 'ignore').decode('gbk'))
+                        else:
+                            filtered_row.append(cell)
+                    write_row_func(filtered_row)
 
 
 def acl_audit(object_type):
+    # 在屏幕上输出当前要审计的对象类型
+    print(f"[+] 正在审计 {object_type} 对象ACL...")
     '''
     默认高权限组无需输出
-    :param object_type: 要审计的对象类型（例如'group'，'user', 'computer', 'ou'等）
+    :param object_type: 要审计的对象类型（例如'group'，'user','domain', 'computer', 'ou'等）
     :return: None
     '''
     # 定义过滤列表，用于过滤掉不需要输出的安全主体
@@ -193,11 +187,12 @@ def acl_audit(object_type):
     # 结果写入csv：
     header = ['Object Name', 'Security Principal', 'Right', 'Principal Type']
     file_name = f"{object_type.capitalize()}_Acl_Info.csv"
+    log_file_name = f"{object_type.capitalize()}_Acl_Info.log"
 
     # 从缓存中读取JSON数据
     objects = get_json_data(object_type)
 
-
+    log_content = ""
     for obj in objects:
         acl_exists = False
         rows_to_write = []
@@ -206,24 +201,27 @@ def acl_audit(object_type):
             ace_obj = obj_info({'PrincipalSID': ace['PrincipalSID'], 'PrincipalType': ace['PrincipalType']})
             if ace_obj and not any(ace_obj['name'].lower().startswith(x) for x in sid_filter):
                 if not acl_exists:
-                    # 如果找到了ACL，则打印对象的名称和分隔符
-                    print("=" * 50)
-                    print(f"{object_type.capitalize()}对象:\t{obj['Properties']['name']}")
+                    # 如果找到了ACL，则添加对象的名称和分隔符到日志内容中
+                    log_content += "=" * 50 + '\n'
+                    log_content += f"{object_type.capitalize()}对象:\t{obj['Properties']['name']}\n"
                     acl_exists = True
                 obj_name = ace_obj['name'] if ace_obj else 'N/A'
                 if not any(obj_name.lower().startswith(x) for x in sid_filter):
                     rows_to_write.append([obj['Properties']['name'], obj_name, ace['RightName'], ace['PrincipalType']])
-                    # 打印ACE信息
-                    print(f"ACE:\n安全主体:\t{obj_name}\n权限:\t{ace['RightName']}\n安全主体类型:\t{ace['PrincipalType']}\n")
+                    # 添加ACE信息到日志内容中
+                    log_content += f"ACE:\n安全主体:\t{obj_name}\n权限:\t{ace['RightName']}\n安全主体类型:\t{ace['PrincipalType']}\n\n"
 
         if rows_to_write:
-            # 创建output目录
+            write_csv(file_name, header, rows_to_write, encoding="gbk")
 
-            write_csv(file_name, header, rows_to_write)
-
+    # 将日志内容写入文件
+    write_file(log_file_name, log_content)
+    
 
 ## 获取计算机 'RegistrySessions', 'DcomUsers','RemoteDesktopUsers', 'LocalAdmins', 'PSRemoteUsers', 'Sessions', 'PrivilegedSessions' 等信息
+## 获取计算机 'RegistrySessions', 'DcomUsers','RemoteDesktopUsers', 'LocalAdmins', 'PSRemoteUsers', 'Sessions', 'PrivilegedSessions' 等信息
 def get_computer_sessions_info():
+    print("[+] 正在获取计算机Sessions 信息")
     session_keys = [
         'RegistrySessions', 'DcomUsers', 'LocalAdmins',
         'PSRemoteUsers', 'Sessions', 'PrivilegedSessions'
@@ -241,9 +239,9 @@ def get_computer_sessions_info():
                 'PrincipalType': 'Computer',
                 'PrincipalSID': session.get('ComputerSID')
             })['name']
-            print(f"\t用户: {user:<20} 计算机: {computer:<20}")
+            return f"\t用户: {user:<20} 计算机: {computer:<20}\n"
         except Exception as e:
-            print(f"\tAn error occurred: {e}")
+            return f"\tAn error occurred: {e}\n"
 
     def print_other_session(session):
         try:
@@ -252,26 +250,36 @@ def get_computer_sessions_info():
                 'PrincipalType': session['ObjectType']
             })
             if object_info:
-                print(f"\t{object_info['name']:<40}")
+                return f"\t{object_info['name']:<40}\n"
         except Exception as e:
-            print(f"\tAn error occurred: {e}")
+            return f"\tAn error occurred: {e}\n"
 
+    log_file_name = 'computer_sessions_info.log'
+    log_content = ""
     for obj in objects:
         for session_key in session_keys:
             results = obj[session_key]['Results']
             if len(results) > 0:
-                print("=" * 60)
-                print(f"Computer: {obj['Properties']['name']}\t => {session_key}")
+                log_content += "=" * 60 + '\n'
+                log_content += f"Computer: {obj['Properties']['name']}\t => {session_key}\n"
 
                 for session in results:
-                    if session_key in ['RegistrySessions', 'PrivilegedSessions']:
-                        print_registry_or_privileged_session(session)
+                    if session_key in ['RegistrySessions', 'PrivilegedSessions', 'Sessions']:
+                        other_session_output = print_registry_or_privileged_session(session)
+                        if other_session_output:
+                            log_content += other_session_output
                     else:
-                        print_other_session(session)
+                        other_session_output = print_other_session(session)
+                        if other_session_output:
+                            log_content += other_session_output
+
+    write_file(log_file_name, log_content)
+                        
                         
 
 ## 获取所有域用户列表并输出csv
 def get_domain_users():
+    print("[+] 正在获取当前域所有用户信息...")
     object_type = "user"
     user_info_list = []
     
@@ -304,40 +312,49 @@ def get_domain_users():
     write_csv(file_name, header,user_info_list,encoding= 'gbk')
 
 
-## 获取所有计算机列表
+## 获取所有组成员
 def get_group_members():
+    print("[+] 正在获取域所有组内成员...")
     groups = get_json_data("Group")
-    group_info_list = []
-    total_member_count = 0
+    group_csv_data = []  # 用于保存 group 的信息
 
     for group in groups:
-        group_properties = group.get("Properties")
+        group_name = group.get("Properties", {}).get("name")
+        if not group_name:
+            continue
+
         members = group.get("Members", [])
         if not members:
             continue
-        group_info = {"name": group_properties.get("name")}
+
         member_names = [obj_info({"PrincipalType": member["ObjectType"], "PrincipalSID": member["ObjectIdentifier"]}).get("name") for member in members]
-        group_info["members"] = member_names
-        group_info_list.append(group_info)
-        total_member_count += len(members)
 
-    if total_member_count > 0:
-        for group_info in group_info_list:
-            print("=" * 60)
-            print(f"Group: {transform_string(group_info['name'])} => Members:")
-            for member_name in group_info["members"]:
-                print(f"\t{member_name}")
+        for member_name in member_names:
+            group_csv_data.append({"group_name": group_name, "member_name": member_name})
 
+    # 如果 group_csv_data 不为空，则将数据写入 CSV 文件
+    if group_csv_data:
+        file_name = 'group_members.csv'
+        header = ['group_name', 'member_name']
+        write_csv(file_name, header, group_csv_data, encoding='gbk')
+
+        # 新增：定义要写入的文件名和内容
+        log_file_name = 'group_members.log'
+        log_content = ""
+
+        for group in group_csv_data:
+            log_content += "=" * 60 + '\n'
+            log_content += f"Group: {transform_string(group['group_name'])} => Members:" + '\n'
+            log_content += f"\t{group['member_name']}" + '\n'
+
+        # 新增：将内容写入文件
+        write_file(log_file_name, log_content)
+        
 if __name__ == '__main__':
-    '''
-    acl_audit(object_type='user')  # 审计用户对象acl
-    acl_audit(object_type='computer') # 审计计算机对象acl
-    acl_audit(object_type='group') #  审计组对象acl
+    
     get_computer_sessions_info()  # 获取所有计算机登录session
     get_domain_users() # 获取域内所有用户
-    get_group_members() # 获取所有组下面用户
-    '''
-    #acl_audit(object_type='computer')
-    #get_group_members()
-    #get_domain_users()
-    #get_computer_sessions_info()
+    get_group_members() # 获取所有组下面用户 
+    acls = ['domain','user','computer','group'] 
+    for acl in acls:
+        acl_audit(object_type=acl)  
